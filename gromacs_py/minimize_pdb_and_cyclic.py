f3a709f0f62a5c510fd3b9e119aca299a20b3a1a
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2018-11-12 15:38:57 +0100

    Add option for minimize cyclic or not

--- gromacs_py/minimize_pdb_and_cyclic.py
+++ gromacs_py/minimize_pdb_and_cyclic.py
@@ -1,50 +1,60 @@
 #!/usr/bin/env python3
 
-__author__ = "Samuel Murail"
+""" Minimize a pdb file structure
+"""
 
-
-import gromacs.gmx5 as gmx
 import argparse
-from glob import glob
-import os
 import shutil
+import gromacs.gmx5 as gmx
+
+__author__ = "Samuel Murail"
 
 def parser_input():
 
     # Parse arguments :
-    parser = argparse.ArgumentParser(description='Minimize a cyclic peptide structure in 2 steps, the first step without bonds constraints and the second step with bonds constraints')
-    parser.add_argument('-f', action="store", dest="f", help='Input PDB file', type=str, required=True)
-    parser.add_argument('-n', action="store", dest="name", help='Output file name', type=str, required=True)
-    parser.add_argument('-dir', action="store", dest="out_dir", help='Output directory for intermediate files', type=str, default="./tmp")
-    parser.add_argument('-m_steps', action="store", dest="min_steps", help='Minimisation nsteps, default=1000', type=int, default=1000)
-    parser.add_argument('-keep', action="store_true", dest="keep_flag", help='Flag to keep temporary files (without flag output directory will be delete')
+    parser = argparse.ArgumentParser(description='Minimize a cyclic peptide structure in 2 steps, \
+        the first step without bonds constraints and the second step with bonds constraints')
+    parser.add_argument('-f', action="store", dest="f",
+                        help='Input PDB file', type=str, required=True)
+    parser.add_argument('-n', action="store", dest="name",
+                        help='Output file name', type=str, required=True)
+    parser.add_argument('-dir', action="store", dest="out_dir",
+                        help='Output directory for intermediate files', type=str, default="./tmp_em")
+    parser.add_argument('-m_steps', action="store", dest="min_steps",
+                        help='Minimisation nsteps, default=1000', type=int, default=1000)
+    parser.add_argument('-keep', action="store_true", dest="keep_flag",
+                        help='Flag to keep temporary files (without flag output \
+                        directory will be delete')
+    parser.add_argument('-cyclic', action="store_true", dest="cyclic_flag",
+                        help='Flag to indicate if the peptide/protein is cyclic')
 
-    return(parser)
+    return parser
 
 
 if __name__ == "__main__":
 
-    parser = parser_input()
-    args = parser.parse_args()
-
+    my_parser = parser_input()
+    args = my_parser.parse_args()
 
     vsite = "none"
-    
-    cyclic_pep = gmx.GmxSys(name = args.name, coor_file = args.f)
-    cyclic_pep.cyclic_peptide_top(out_folder = args.out_dir+'/top')
-    
-    cyclic_pep.em(out_folder = args.out_dir+'/em',name = "min_"+args.name,
-    	nsteps = args.min_steps, posres = "", nstxout = 1000,create_box_flag = True,
-    	constraints = "none")
-    
-    cyclic_pep.convert_trj( traj=False )
-    
+    peptide = gmx.GmxSys(name=args.name, coor_file=args.f)
+
+    if args.cyclic_flag:
+        peptide.cyclic_peptide_top(out_folder=args.out_dir+'/top')
+    else:
+        peptide.add_top(out_folder=args.out_dir+'/top')
+
+    peptide.em(out_folder=args.out_dir+'/em', name="min_"+args.name,
+               nsteps=args.min_steps, posres="", nstxout=1000, create_box_flag=True,
+               constraints="none")
+
+    peptide.convert_trj(traj=False)
+
     # Get the minimised structure:
-    shutil.copyfile(cyclic_pep.coor_file, args.name+'.pdb')
-    
+    shutil.copyfile(peptide.coor_file, args.name+'.pdb')
+
     # Keep or not the intermediate files:
     if not args.keep_flag:
-    	shutil.rmtree(args.out_dir, ignore_errors=True)
+        shutil.rmtree(args.out_dir, ignore_errors=True)
     else:
-    	cyclic_pep.display()
-    
+        peptide.display()
