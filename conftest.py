06949ed0ef058b8be1a4e7cb137b819d027e258a
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-05-15 18:19:59 +0200

    ignore for test .py files in notebook/

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@
 
 def pytest_ignore_collect(path):
     path_list = str(path).split('/')
-    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] == 'gromacs_py':
+    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] in ['gromacs_py', 'notebook']:
         print('Ignore :',str(path))
         return True
