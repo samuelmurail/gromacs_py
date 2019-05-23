6dbc5efcd25910fcf1409fad90be0aecb5cf06f2
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2019-05-23 14:54:47 +0200

    remove useless import

--- gromacs_py/__init__.py
+++ gromacs_py/__init__.py
@@ -1,13 +1,3 @@
 #!/usr/bin/env python3
 # coding: utf-8
  
-"""
-    
-"""
- 
-__version__ = "1.0.0post2"
- 
-from gromacs_py.gromacs.gmx5 import TopSys, Itp, TopMol, GmxSys
-import gromacs_py.gromacs.tools.os_command
-import gromacs_py.gromacs.tools.pdb_manip
-
