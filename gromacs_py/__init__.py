378ee2c50563a69383d6b8d0116b93e4645fb806
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-05-23 09:27:22 +0200

    Add imports

--- gromacs_py/__init__.py
+++ gromacs_py/__init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# coding: utf-8
  
 """
     
 """
  
-__version__ = "1.0"
+__version__ = "1.0.0post2"
  
+from gromacs_py.gromacs.gmx5 import TopSys, Itp, TopMol, GmxSys
+import gromacs_py.gromacs.tools.os_command
+import gromacs_py.gromacs.tools.pdb_manip
+
