cc833e8ec0c41c99a1dacb9e59b3fe5ab04b8abc
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-05-23 09:26:11 +0200

    Add new name for pdb2pqr BIN

--- gromacs_py/gromacs/tools/pdb2pqr.py
+++ gromacs_py/gromacs/tools/pdb2pqr.py
@@ -23,7 +23,10 @@ except ImportError:
 
 
 PDB2PQR_MOD_DIRNAME = os.path.dirname(os.path.abspath(__file__))
-PDB2PQR_BIN = 'pdb2pqr.py'
+# Add 'pdb2pqr_cli' in case it is installed with conda
+PDB2PQR_BIN = os_command.which('pdb2pqr.py','pdb2pqr_cli')
+
+
 
 # Test folder path
 PQR_LIB_DIR = os.path.dirname(os.path.abspath(__file__))
