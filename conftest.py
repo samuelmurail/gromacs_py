27fcdeb2b54e172765d733a082577d5ea316d3a0
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-10-29 16:03:32 +0100

    Remove ci-helpers for pytest directories

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 
 # Remove all script from 'gromacs_py' directory:
+# Remove all script from 'notebook' directory:
+# Remove all script from 'ci-helpers' directory: (Travis issue)
 
 def pytest_ignore_collect(path):
     path_list = str(path).split('/')
-    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] in ['gromacs_py', 'notebook']:
+    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] in ['gromacs_py', 'notebook', 'ci-helpers']:
         print('Ignore :',str(path))
         return True
