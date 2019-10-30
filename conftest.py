4719a98257a09989ae6bae4d99cdf688f5f31076
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-10-30 01:06:40 +0100

    Add -s -vvv option to pytest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 
 # Remove all script from 'gromacs_py' directory:
 # Remove all script from 'notebook' directory:
-# Remove all script from 'ci-helpers' directory: (Travis issue)
+# Remove all script from 'ci-helpers' directory: (Travis issue) Seems useless
 
 def pytest_ignore_collect(path):
     path_list = str(path).split('/')
-    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] in ['gromacs_py', 'notebook', 'ci-helpers']:
+    if path_list[-1]!='__init__.py' and path_list[-1][-3:]=='.py' and path_list[-2] in ['gromacs_py', 'notebook']:
         print('Ignore :',str(path))
         return True
