8c6fe77449d28e1248f4cda42dab2c6a40828488
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2020-11-10 18:25:13 +0100

    Add Condition of gromacs version for peptide creation

--- gromacs_py/test/test_GmxSys.py
+++ gromacs_py/test/test_GmxSys.py
@@ -27,17 +27,16 @@ __email__ = "samuel.murail@u-paris.fr"
 __status__ = "Production"
 
 
-def test_insert_mol(tmp_path):
+def test_insert_ethanol(tmp_path):
 
     gromacs_py.gmx.show_log()
     ##################################
     # ##   Create the topologie:   ###
     ##################################
     prot = GmxSys(name='1y0m', coor_file=PDB_1Y0M)
-    prot.prepare_top(out_folder=os.path.join(tmp_path, 'top_SH3'),
-                     vsite='hydrogens')
+    prot.prepare_top(out_folder=os.path.join(tmp_path, 'top_SH3'))
     top_coor = pdb_manip.Coor(prot.coor_file)
-    assert top_coor.num == 1064
+    assert top_coor.num == 996
 
     prot.solvate_add_ions(out_folder=os.path.join(tmp_path, 'top_sys'))
 
@@ -45,7 +44,7 @@ def test_insert_mol(tmp_path):
             nsteps=10,
             constraints='none')
     em_coor = pdb_manip.Coor(prot.coor_file)
-    assert em_coor.num == 15337
+    assert em_coor.num == 15383
 
     ener_pd = prot.get_ener(['Potential'])
     assert ener_pd['Potential'].values[-1] < -155000
@@ -83,129 +82,54 @@ def test_insert_mol(tmp_path):
 
     prot.display_history()
 
-    """
-    >>> ################################
-    >>> ####   Minimize the system   ###
-    >>> ################################
-    >>> prot.em_2_steps(out_folder=os.path.join(tmp_path, 'top_D_SH3'), \
-no_constr_nsteps=10, constr_nsteps=10)
-    - Create the tpr file Init_em_1y0m.tpr
-    gmx grompp -f Init_em_1y0m.mdp -c SH3_D_neutral.gro -r SH3_D_neutral.gro \
--p SH3_D_neutral.top -po out_Init_em_1y0m.mdp -o Init_em_1y0m.tpr -maxwarn 1
-    - Launch the simulation Init_em_1y0m.tpr
-    gmx mdrun -s Init_em_1y0m.tpr -deffnm Init_em_1y0m -nt 0 -ntmpi 0 \
--nsteps -2 -nocopyright
-    - Create the tpr file 1y0m.tpr
-    gmx grompp -f 1y0m.mdp -c Init_em_1y0m.gro -r Init_em_1y0m.gro -p \
-SH3_D_neutral.top -po out_1y0m.mdp -o 1y0m.tpr -maxwarn 1
-    - Launch the simulation 1y0m.tpr
-    gmx mdrun -s 1y0m.tpr -deffnm 1y0m -nt 0 -ntmpi 0 -nsteps -2 -nocopyright
-    >>> ##################################
-    >>> ####    Show system history    ###
-    >>> ##################################
-    >>> prot.display_history() #doctest: +ELLIPSIS
-    State -3:
-    <BLANKLINE>
-    name         : 1y0m
-    sim_name     : genion_1y0m_water_ion
-    coor_file    : .../top_sys/1y0m_water_ion.gro
-    top_file     : .../top_sys/1y0m_water_ion.top
-    tpr          : .../top_sys/genion_1y0m_water_ion.tpr
-    mdp          : ...template/mini.mdp
-    nt           : 0
-    ntmpi        : 0
-    sys_history  : 0
-    <BLANKLINE>
-    State -2:
-    <BLANKLINE>
-    name         : 1y0m
-    sim_name     : genion_SH3_D_neutral
-    coor_file    : .../top_D_SH3/SH3_D_neutral.gro
-    top_file     : .../top_D_SH3/SH3_D_neutral.top
-    tpr          : .../top_D_SH3/genion_SH3_D_neutral.tpr
-    mdp          : ...template/mini.mdp
-    xtc          : .../em_SH3/1y0m.trr
-    edr          : .../em_SH3/1y0m.edr
-    log          : .../em_SH3/1y0m.log
-    nt           : 0
-    ntmpi        : 0
-    sys_history  : 0
-    <BLANKLINE>
-    State -1:
-    <BLANKLINE>
-    name         : 1y0m
-    sim_name     : Init_em_1y0m
-    coor_file    : .../top_D_SH3/Init_em_1y0m.gro
-    top_file     : .../top_D_SH3/SH3_D_neutral.top
-    tpr          : .../top_D_SH3/Init_em_1y0m.tpr
-    mdp          : .../top_D_SH3/Init_em_1y0m.mdp
-    xtc          : .../top_D_SH3/Init_em_1y0m.trr
-    edr          : .../top_D_SH3/Init_em_1y0m.edr
-    log          : .../top_D_SH3/Init_em_1y0m.log
-    nt           : 0
-    ntmpi        : 0
-    sys_history  : 0
-    <BLANKLINE>
-    >>> ###################################
-    >>> ####   Equilibrate the system   ###
-    >>> ###################################
-    >>> equi_template_mdp = os.path.join(GROMACS_MOD_DIRNAME, \
-"template/equi_vsites.mdp")
-    >>> mdp_options = {'nsteps': 100, 'define': '-DPOSRES', 'dt': 0.001}
-    >>> prot.run_md_sim(out_folder=os.path.join(tmp_path, 'equi_HA_D_SH3'), \
-name="equi_HA_D_SH3", mdp_template=equi_template_mdp,\
-                        mdp_options=mdp_options)
-    - Create the tpr file equi_HA_D_SH3.tpr
-    gmx grompp -f equi_HA_D_SH3.mdp -c ../top_D_SH3/1y0m.gro -r \
-../top_D_SH3/1y0m.gro -p ../top_D_SH3/SH3_D_neutral.top -po \
-out_equi_HA_D_SH3.mdp -o equi_HA_D_SH3.tpr -maxwarn 0
-    - Launch the simulation equi_HA_D_SH3.tpr
-    gmx mdrun -s equi_HA_D_SH3.tpr -deffnm equi_HA_D_SH3 -nt 0 -ntmpi 0 \
--nsteps -2 -nocopyright
-    >>> prot.get_simulation_time() #doctest: +ELLIPSIS
-    - Get simulation time from : .../equi_HA_D_SH3/equi_HA_D_SH3.cpt
-    gmx check -f .../equi_HA_D_SH3/equi_HA_D_SH3.cpt
-    0.1
-    >>> prot.convert_trj(traj=False) #doctest: +ELLIPSIS
-    - Convert trj/coor
-    gmx trjconv -f .../equi_HA_D_SH3/equi_HA_D_SH3.gro -o \
-.../equi_HA_D_SH3/equi_HA_D_SH3_compact.pdb -s \
-.../equi_HA_D_SH3/equi_HA_D_SH3.tpr -ur compact -pbc mol
-    >>> prot.display() #doctest: +ELLIPSIS
-    name         : 1y0m
-    sim_name     : equi_HA_D_SH3
-    coor_file    : .../equi_HA_D_SH3/equi_HA_D_SH3_compact.pdb
-    top_file     : .../top_D_SH3/SH3_D_neutral.top
-    tpr          : .../equi_HA_D_SH3/equi_HA_D_SH3.tpr
-    mdp          : .../equi_HA_D_SH3/equi_HA_D_SH3.mdp
-    xtc          : .../equi_HA_D_SH3/equi_HA_D_SH3.xtc
-    edr          : .../equi_HA_D_SH3/equi_HA_D_SH3.edr
-    log          : .../equi_HA_D_SH3/equi_HA_D_SH3.log
-    nt           : 0
-    ntmpi        : 0
-    sys_history  : 4
-    >>> #########################################
-    >>> ### Extract Potential Energy and Temp ###
-    >>> #########################################
-    >>> ener_pd = prot.get_ener(['Potential', 'Temp'])  #doctest: +ELLIPSIS
-    - Extract energy
-    gmx energy -f .../equi_HA_D_SH3/equi_HA_D_SH3.edr -o tmp_edr.xvg
-    >>> ener_pd['Potential'].mean() #doctest: +ELLIPSIS
-    -2...
-    >>> rmsd_pd = prot.get_rmsd(['C-alpha', 'Protein'])  #doctest: +ELLIPSIS
-    - Extract RMSD
-    - Create the ndx file ...equi_HA_D_SH3.ndx
-    gmx make_ndx -f ...equi_HA_D_SH3_compact.pdb -o ...equi_HA_D_SH3.ndx
-    gmx rms -s ...equi_HA_D_SH3.tpr -f ...equi_HA_D_SH3.xtc -n ...\
-equi_HA_D_SH3.ndx -o tmp_rmsd.xvg -fit rot+trans -ng 1 -pbc no
-    >>> rmsd_pd #doctest: +ELLIPSIS
-       time ...Protein
-    0   0.0...
-    >>> rmsf_pd = prot.get_rmsf(['Protein'], res="yes")  #doctest: +ELLIPSIS
-    - Extract RMSF
-    gmx rmsf -s ...equi_HA_D_SH3.tpr -f ...equi_HA_D_SH3.xtc -n ...\
-equi_HA_D_SH3.ndx -o tmp_rmsf.xvg -fit no -res yes
-    >>> rmsf_pd #doctest: +ELLIPSIS
-        Residue    RMSF
-    0       791  ...
-    """
+@pytest.mark.skipif(float(gromacs_py.gmx.gmx_version) >= 2019,
+                    reason="Gromacs verions >= 19 have issues with ACE")
+def test_insert_peptide_vsite(tmp_path):
+
+    gromacs_py.gmx.show_log()
+    ##################################
+    # ##   Create the topologie:   ###
+    ##################################
+    prot = GmxSys(name='1y0m', coor_file=PDB_1Y0M)
+    prot.prepare_top(out_folder=os.path.join(tmp_path, 'top_SH3'),
+                     vsite='hydrogens')
+    top_coor = pdb_manip.Coor(prot.coor_file)
+    assert top_coor.num == 1064
+
+    prot.solvate_add_ions(out_folder=os.path.join(tmp_path, 'top_sys'))
+
+    prot.em(out_folder=os.path.join(tmp_path, 'em_SH3'),
+            nsteps=10,
+            constraints='none')
+    em_coor = pdb_manip.Coor(prot.coor_file)
+    assert em_coor.num == 15337
+
+    ener_pd = prot.get_ener(['Potential'])
+    assert ener_pd['Potential'].values[-1] < -155000
+
+    ###################################
+    # ##    Create a DD peptide     ###
+    ###################################
+    pep = GmxSys(name='DD')
+    pep.create_peptide(sequence='DD',
+                       out_folder=os.path.join(tmp_path, 'top_DD'),
+                       em_nsteps=10, equi_nsteps=0,
+                       vsite='hydrogens')
+
+    ####################################################
+    # # Insert 4 copy of ethanol in  the SH3 system: ###
+    ####################################################
+
+    prot.insert_mol_sys(mol_gromacs=pep, mol_num=10,
+                        new_name='SH3_DD',
+                        out_folder=os.path.join(tmp_path, 'top_DD_SH3'))
+    ################################
+    # ##   Minimize the system   ###
+    ################################
+    prot.em_2_steps(out_folder=os.path.join(tmp_path, 'em_DD_SH3'),
+                    no_constr_nsteps=10, constr_nsteps=10)
+    prot_lig_coor = pdb_manip.Coor(prot.coor_file)
+    assert 15300 < prot_lig_coor.num < 15518
+
+    prot.display_history()
+
