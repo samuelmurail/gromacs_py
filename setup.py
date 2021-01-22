ffd901cf9214b479cc3df73546e793e230158aee
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2021-01-22 14:28:55 +0100

    Update pdb_manip_py version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "matplotlib",
         "os_command_py",
         "pdb2pqr_htmd_propka30>=0.0.4",
-        "pdb_manip_py>=1.3.5",
+        "pdb_manip_py>=1.3.6",
         "tqdm",
     ],
     package_data={
