b7a5762178e1fb4019c0bbcd3264a7a822843a39
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2021-04-20 00:32:53 +0200

    Bump version: 1.2.1 → 2.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gromacs_py',
-    version='1.2.1',
+    version='2.0.0',
     author='Samuel Murail',
     author_email='samuel.murail@u-paris.fr',
     packages=find_packages(),
